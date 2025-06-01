afe4ff213c6f689a7e82bd561a1834de0bd1cd66
Aldric <s3949508@student.rmit.edu.au>
2025-06-02 03:40:05 +1000

    Add BasicArithmeticOperations program for basic arithmetic calculations

---
+++ practice_basics/BasicArithmeticOperations/BasicArithmeticOperations.java
@@ -0,0 +1,20 @@
+package practice_basics.BasicArithmeticOperations;
+
+import java.util.Scanner;
+
+public class BasicArithmeticOperations{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int numA = sc.nextInt();
+        int numB = sc.nextInt();
+
+        System.out.println(numA + " + " + numB + " = " + (numA + numB));
+        System.out.println(numA + " - " + numB + " = " + (numA - numB));
+        System.out.println(numA + " * " + numB + " = " + (numA * numB));
+        System.out.println(numA + " / " + numB + " = " + (numA / numB));
+        System.out.println(numA + " % " + numB + " = " + (numA % numB));
+        
+        sc.close();
+    }
+}
