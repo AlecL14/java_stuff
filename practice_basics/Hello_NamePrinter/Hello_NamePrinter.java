c40a44ae0e3b905b0955fe923db38ddffb93ade8
Aldric <s3949508@student.rmit.edu.au>
2025-06-02 02:30:39 +1000

    A simple Hello and Name printer

---
+++ practice_basics/Hello_NamePrinter/Hello_NamePrinter.java
@@ -0,0 +1,7 @@
+package practice_basics.Hello_NamePrinter;
+
+public class Hello_NamePrinter{
+    public static void main(String args[]){
+        System.out.println("Hello" + "\n" + "Aldric Lim");
+    }
+}
