cfe8b1de98586f081344b5e7541c736a3ba4fdb3
Aldric <aldric_xavier03hotmail.com>
2025-06-02 05:03:23 +1000

    Add SwapVariables program to demonstrate variable swapping

---
+++ src/practice_basics/SwapVariables/SwapVariables.java
@@ -0,0 +1,16 @@
+package src.practice_basics.SwapVariables;
+
+public class SwapVariables {
+    public static void main(String[] args) {
+        int[] swapped = swap(1, 2);
+        System.out.println("x = " + swapped[0] + "\n" 
+                            + "y = " + swapped[1]);
+    }
+
+    public static int[] swap(int x, int y){
+        int tmp = x;
+        x = y;
+        y = tmp;
+        return new int[]{x, y};
+    }
+}
